832887dd152d10099b83a3a7e8fd864abc7887da
Ryno Lawson <unleashedracing@gmail.com>
2017-06-09 10:11:24 +0200

    Organising project, project plan and data

---
+++ data/ryno_project/parse_test.py
@@ -0,0 +1,7 @@
+#Parsing of this file
+#./data/ryno_project/P3764_P1456.LMO.indiv.MAGs.merged.raw_counts.tsv
+../data/ryno_project/P3764_P1456.LMO.indiv.MAGs.merged.raw_counts.tsv
+
+
+f = open('P3764_P1456.LMO.indiv.MAGs.merged.raw_counts.tsv', 'r+')
+print(f)
